0fcfc0d19da771801934c4a8a2167476a1514cdc
dnjoe96 <donjoedbest@gmail.com>
2022-05-10 20:33:11 +0100

    Define api views

---
+++ app/v1/views/barber_views.py
@@ -0,0 +1,77 @@
+from app.v1.views import app_views
+from app.v1.models.ops import Comments
+from app.v1.models.user import User
+from app.v1.models.barber import Barber, BarberRating, Style
+
+
+@app_views.route('/user/barber/', methods=['POST'])
+def create_barber():
+    pass
+
+
+@app_views.route('/user/barber/activate/<id>', methods=['PUT'])
+def create_barber():
+    """
+        This function activates a barber's account
+    :return:
+    """
+    pass
+
+
+@app_views.route('/user/barber/', methods=['GET'])
+def get_barbers():
+    """
+        This function gets all barbers enrolled
+    :return:
+    """
+    pass
+
+
+@app_views.route('/user/barber/<id>', methods=['GET'])
+def get_a_barber(id):
+    """
+        This function get a particular user by it's id
+    :param id: unique id of a barber
+    :return:
+    """
+    pass
+
+
+@app_views.route('/user/barber/<id>', methods=['PUT'])
+def edit_barber(id):
+    pass
+
+
+@app_views.route('/user/barber/', methods=['DEL'])
+def delete_a_barber():
+    pass
+
+
+@app_views.route('/barbers', methods=['GET'])
+def get_barbers_by_location():
+    """
+    the function will return all barbers that exactly or closely match the location
+    data passed in json
+    :return:
+    """
+    pass
+
+
+@app_views.route('/barber/select-style', methods=['POST'])
+def select_styles():
+    """
+        The function will enable a barber to select the styles from a list of styles
+        by their id in a list. This styles will be saved in the database for this barber.
+    :return:
+    """
+    pass
+
+
+@app_views.route('/barber/unselect-style', methods=['POST'])
+def unselect_a_styles():
+    """
+        The function will enable a barber to unselect the styles from a list of styles
+        by their ids. This styles will be removed from the database for this barber.
+    :return:
+    """
+    pass
--- app/v1/views/user_views.py
+++ app/v1/views/user_views.py
@@ -1,8 +1,39 @@
 # from .v1 import app_views
 # from api.v1 import app_views
 from app.v1.views import app_views
-
+from app.v1.models.ops import Comments
+from app.v1.models.user import User
+from app.v1.models.barber import Barber, BarberRating, Style
 
 @app_views.route('/')
 def index():
+    """
+        Test view point to ensure that the endpoint is connecting
+    :return:
+    """
     return 'Hello Africa!'
+
+
+@app_views.route('/user/cust/', methods=['POST'])
+def create_a_cust():
+    pass
+
+
+@app_views.route('/user/cust/', methods=['GET'])
+def get_custs():
+    pass
+
+
+@app_views.route('/user/cust/<id>', methods=['GET'])
+def get_a_cust(id):
+    pass
+
+
+@app_views.route('/user/cust/<id>', methods=['PUT'])
+def edit_a_cust(id):
+    pass
+
+
+@app_views.route('/user/cust/', methods=['DEL'])
+def delete_a_use():
+    pass
