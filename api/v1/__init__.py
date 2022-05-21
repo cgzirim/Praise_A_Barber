e7fe0d97d97b307b4747f8f408672e18d531e033
iChigozirim <iamokwukwe@gmail.com>
2022-05-21 15:23:18 +0100

    Add Flask Swagger configs

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -4,14 +4,31 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from flask import jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
+from flask_swagger_ui import get_swaggerui_blueprint
 
 # from api.v1.views import app_views
 
-
+# Instantiate flask app
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://0.0.0.0:*"}})
+
+# Set configs
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Swagger configs
+SWAGGER_URL = '/api/v1/docs'
+API_URL = '/static/swagger.json'
+SWAGGER_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Praise A Barber Api"
+    }
+)
+app.register_blueprint(SWAGGER_BLUEPRINT, url_prefix = SWAGGER_URL)
+
+# Instantiate db object
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
