3ef73c7833ba5ee869aad2ccc738b5b1eedb06b5
Chigozirim <iamokwukwe@gmail.com>
2022-05-26 18:43:19 +0100

    Add columns: created_at, updated_at. Add method - to_dict()

--- models/ops.py
+++ models/ops.py
@@ -1,12 +1,26 @@
 from api.v1 import db
+from datetime import datetime
 
 
-class Comments(db.Model):
+class Review(db.Model):
     id = db.Column(db.String(50), primary_key=True)
-    comment = db.Column(db.Text)
-    user_id = db.Column(db.String(100), db.ForeignKey('user.id'), nullable=False)
-    barber_id = db.Column(db.String(100), db.ForeignKey('barber.id'), nullable=False)
+    review = db.Column(db.Text)
+    user_id = db.Column(db.String(100), db.ForeignKey("user.id"), nullable=False)
+    barber_id = db.Column(db.String(100), db.ForeignKey("barber.id"), nullable=False)
+    created_date = db.Column(db.DateTime, default=datetime.utcnow())
+    updated_date = db.Column(db.DateTime, default=datetime.utcnow())
+
+    def to_dict(self):
+        """Returns a dictionary containing informatioin about a review."""
+        new_dict = {
+            "id": self.id,
+            "review": self.review,
+            "user_id": self.user_id,
+            "barber_id": self.barber_id,
+            "created_date": self.created_date,
+            "updated_date": self.updated_date,
+        }
+        return new_dict
 
     def __repr__(self):
-        return '<Comment %r>' % self.id
-
+        return "<Review %r>" % self.id
