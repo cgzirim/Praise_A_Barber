faa58872f71a7c998d2425cb7d737cb5d7dda015
Chigozirim <iamokwukwe@gmail.com>
2022-05-26 18:51:08 +0100

    Add column - updated_date. Update column comments

--- models/user.py
+++ models/user.py
@@ -10,19 +10,22 @@ class User(db.Model):
     email = db.Column(db.String(80), unique=True)
     password = db.Column(db.String(255), unique=True)
     usage_count = db.Column(db.Integer, default=0)
-    signup_date = db.Column(db.DateTime, default=datetime.utcnow())  # this should have a default value.
-    comments = db.relationship('Comments', backref='user', lazy=True)
+    signup_date = db.Column(
+        db.DateTime, default=datetime.utcnow()
+    )  # this should have a default value.
+    update_date = db.Column(db.DateTime)
+    reviews = db.relationship("Review", backref="user", lazy=True)
 
     def to_dict(self):
         """Returns a dictionary containing a barber's information."""
         new_dict = self.__dict__.copy()
 
-        if 'password' in new_dict:
-            new_dict.pop('password')
+        if "password" in new_dict:
+            new_dict.pop("password")
 
-        new_dict.pop('_sa_instance_state')
+        new_dict.pop("_sa_instance_state")
 
         return new_dict
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return "<User %r>" % self.username
